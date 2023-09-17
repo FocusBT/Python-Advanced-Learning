cd8d34552e811842a8e280c31e2575554033a455
FocusBT <wasee62313@gmail.com>
2023-09-17 19:08:59 +0500

    understood response model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # from typing import Union
-from fastapi import FastAPI, Query, Path, Body
-from pydantic import BaseModel, Field, HttpUrl
+from fastapi import FastAPI, Query, Path, Body, Cookie, Header
+from pydantic import BaseModel, Field, HttpUrl, EmailStr
 from typing import Optional
 from enum import Enum
 app = FastAPI()
@@ -124,12 +124,63 @@ def index():
 
 
 # Part 10 -> Declare Request Example Data
-class Item(BaseModel):
-    name: str = Field(..., example="wase")
-    id: int = Field(..., example=12)
-    des: str = Field(..., max_length=300, example="desc")
+# class Item(BaseModel):
+#     name: str = Field(..., example="wase")
+#     id: int = Field(..., example=12)
+#     des: str = Field(..., max_length=300, example="desc")
+# # in the each field you can give an example that will be shown in swagger docs
+# # there is another way as wel in the function defination just add example and the whole json input
+# # async def getItems(item_id: int, item: Item = Body(..., example={"name": "wasi", "id": 12, "des": "this is desc"})):
+# @app.post("/items/{item_id}")
+# async def getItems(item_id: int, item: Item):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+# # or there is one more method to do that we can use examples and make a dict,
+# # examples={
+#     # "normal": {
+#     #     "name": "wasi",
+#     #     "id": 12,
+#     #     "des": "this is normal desc"
+#     # },
+#     # "extreme": {
+#     #     "name": "wasay",
+#     #     "id": 128,
+#     #     "des": "this is extemere"
+#     # }
+# # }
 
-@app.post("/items/{item_id}")
-async def getItems(item_id: int, item: Item):
-    results = {"item_id": item_id, "item": item}
-    return results
+# Part 12 -> Cookie and header
+# @app.get("/items")
+# async def read_items(
+#     cookie_id: str | None = Cookie(None),
+#     accept_encoding: str | None = Header(None),
+#     user_agent: str | None = Header(None)
+# ):
+#     return {
+#         "cookie_id": cookie_id,
+#         "accept_encoding":accept_encoding,
+#         "user_agent": user_agent
+    # }
+
+
+# Part 13 -> Response modal
+
+class UserBase(BaseModel):
+    user: str
+    email: str
+    full_name: str | None = None
+# API will validate the response against the UserIn model and ensure that 
+# it conforms to the specified structure and data types.
+class UserIn(UserBase):
+    password: str
+
+class UserOut(UserBase):
+    pass
+
+
+# the UserOut defines what will be out output, and it is also shown in swagger docs
+# you can see what will be input schema and what will be output schema
+# or you can use response_modal_include or response modal_exclude to return the data you want
+@app.post("/user/", response_model=UserOut, response_model_include={"name, email"})
+async def create_user(user: UserIn):
+    return user
